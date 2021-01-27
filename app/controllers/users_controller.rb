4b1a898197bbe1f06f6be12b565f0510d77f9367
OdongAlican <sandieo.2020@gmail.com>
2021-01-27 11:52:04 +0300

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
+  before_action :set_user, only: %i[show update destroy]
   # POST /signup
   # return authenticated token upon signup
   def create
@@ -9,6 +10,10 @@ class UsersController < ApplicationController
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token, user: user }
     json_response(response, :created)
+  end
+
+  def show
+    json_response(@user)
   end
 
   private
@@ -20,5 +25,9 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
