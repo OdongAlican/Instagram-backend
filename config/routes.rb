cb490d5f88b5cdb373bebaa7fa7bfa466c2c058d
OdongAlican <sandieo.2020@gmail.com>
2021-01-27 11:54:04 +0300

    add posts and photo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  resources :users, only: %i[show update]
+
+  resources :posts, only: %i[index show create] do
+    resources :photos, only: [:create]
+  end
 end
