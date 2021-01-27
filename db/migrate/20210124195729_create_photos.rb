ea800f132d7aa44c29d5dbe8a13ca2676d10a2df
OdongAlican <sandieo.2020@gmail.com>
2021-01-27 11:42:47 +0300

    update migrations

---
+++ db/migrate/20210124195729_create_photos.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreatePhotos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photos do |t|
+      t.string :image
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_105_150_149) do
+ActiveRecord::Schema.define(version: 20_210_124_195_729) do
+  create_table 'photos', force: :cascade do |t|
+    t.string 'image'
+    t.integer 'post_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_photos_on_post_id'
+  end
+
+  create_table 'posts', force: :cascade do |t|
+    t.string 'content'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
@@ -20,4 +36,7 @@ ActiveRecord::Schema.define(version: 20_210_105_150_149) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  add_foreign_key 'photos', 'posts'
+  add_foreign_key 'posts', 'users'
 end
