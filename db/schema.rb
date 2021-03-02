76f8a4730c37679f80c3294bafe5d1f0f0077079
OdongAlican <sandieo.2020@gmail.com>
2021-03-02 12:24:05 +0300

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_124_195_729) do
+ActiveRecord::Schema.define(version: 20_210_301_212_202) do
+  create_table 'likes', force: :cascade do |t|
+    t.integer 'post_id', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_likes_on_post_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
+  end
+
   create_table 'photos', force: :cascade do |t|
     t.string 'image'
     t.integer 'post_id', null: false
@@ -37,6 +46,8 @@ ActiveRecord::Schema.define(version: 20_210_124_195_729) do
     t.datetime 'updated_at', precision: 6, null: false
   end
 
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users'
   add_foreign_key 'photos', 'posts'
   add_foreign_key 'posts', 'users'
 end
