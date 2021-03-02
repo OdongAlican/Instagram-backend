22b8b6e42e021305fef9ed61965302cfbda5896b
OdongAlican <sandieo.2020@gmail.com>
2021-03-02 12:28:01 +0300

    add likes association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :email, :password_digest
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
