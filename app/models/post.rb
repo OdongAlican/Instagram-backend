c51c277f6b57e5227f2e5b51ca4ab59205d0954f
OdongAlican <sandieo.2020@gmail.com>
2021-03-02 12:26:50 +0300

    add is_liked method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+
+  has_many :likes, -> { order('created_at desc') }
+
+  def is_liked(user)
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
