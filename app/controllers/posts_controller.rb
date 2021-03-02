11ae743f2ecb84e55749ecd49ffb5c9b6e7241ce
OdongAlican <sandieo.2020@gmail.com>
2021-03-02 12:25:11 +0300

    Add the following - Add find post private method - Add show method - Add destroy method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show destroy]
+
   def index
     @posts = Post.all.limit(10).order('created_at DESC')
     @post = Post.new
-    data = @posts.to_json({ include: %i[user photos] })
+    data = @posts.to_json({ include: %i[user photos likes] })
     json_response(data, :created)
   end
 
@@ -21,7 +23,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @photos = @post.photos
+    @likes = @post.likes.to_json({ include: ['user'] })
+    @is_liked = @post.is_liked(current_user)
+  end
+
+  def destroy
+    if @post.user = current_user
+      if @post.destroy
+        data = { message: 'post destroyed' }
+        json_response(data, :no_content)
+      else
+        data = { message: 'Something went wrong' }
+        json_response(data, :no_content)
+      end
+    else
+      data = { message: "You don't have permission to delete this Post" }
+      json_response(data, :no_content)
+    end
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:content)
