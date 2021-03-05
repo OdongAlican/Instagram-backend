ec24261d8a5add344549b6d742a4f6c64be32c2d
OdongAlican <sandieo.2020@gmail.com>
2021-03-05 08:23:00 +0300

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.limit(10).order('created_at DESC')
     @post = Post.new
-    data = @posts.to_json({ include: %i[user photos likes] })
+    data = @posts.to_json({ include: ['user', 'photos', { likes: { include: 'user' } }, { comments: { include: 'user' } }] })
     json_response(data, :created)
   end
 
@@ -24,10 +24,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    # @photos = @post.photos
-    # @likes = @post.likes.to_json({ include: ['user'] })
     @is_liked = @post.is_liked(current_user)
-    @result = @post.to_json({ include: %w[user likes photos] })
+    @result = @post.to_json({ include: %i[user likes photos comments] })
     json_response(@result, :created)
   end
 
@@ -35,14 +33,14 @@ class PostsController < ApplicationController
     if @post.user = current_user
       if @post.destroy
         data = { 'message' => 'post destroyed' }
-        return json_response(data, :no_content)
+        json_response(data, :no_content)
       else
         data = { 'message' => 'Something went wrong' }
-        return json_response(data, :no_content)
+        json_response(data, :no_content)
       end
     else
-      data = { message: "You don't have permission to delete this Post" }
-      return json_response(data, :no_content)
+      data = { message: 'Not Authorized' }
+      json_response(data, :unauthorized)
     end
   end
 
