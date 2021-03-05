5286c05cb707a1e306201e04ff66c06ffc1aab49
OdongAlican <sandieo.2020@gmail.com>
2021-03-05 22:19:13 +0300

    add is_bookmarked method

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,13 @@ class Post < ApplicationRecord
 
   has_many :likes, -> { order('created_at desc') }
   has_many :comments, -> { order('created_at desc') }
+  has_many :bookmarks, -> { order('created_at desc') }
 
   def is_liked(user)
     Like.find_by(user_id: user.id, post_id: id)
+  end
+
+  def is_bookmarked(user)
+    Bookmark.find_by(user_id: user.id, post_id: id)
   end
 end
