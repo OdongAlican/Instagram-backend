bca330d322f4e61f3e59f525a4340b736494c989
OdongAlican <sandieo.2020@gmail.com>
2021-03-06 17:24:38 +0300

    add follow migration

---
+++ db/migrate/20210305202450_create_follows.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateFollows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :follows do |t|
+      t.integer :follower_id
+      t.integer :followee_id
+
+      t.timestamps
+    end
+  end
+end
