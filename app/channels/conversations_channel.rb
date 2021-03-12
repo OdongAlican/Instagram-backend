769c85790c4e666fa3056eb2235c1d0eb4bc6277
OdongAlican <sandieo.2020@gmail.com>
2021-03-12 14:48:13 +0300

    add conversation channel

---
+++ app/channels/conversations_channel.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ConversationsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'conversations_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
