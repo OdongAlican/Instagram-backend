37cf9a86b99e22091ca9c1473376103db00e25c7
OdongAlican <sandieo.2020@gmail.com>
2021-03-28 22:51:06 +0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,16 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  has_many :conversations, dependent: :destroy
 
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
   has_many :followers, through: :following_users
+
+  def self.nonFollowers(id)
+    user =  User.find(id)
+    users = User.all
+    users - user.followees
+  end
 end
