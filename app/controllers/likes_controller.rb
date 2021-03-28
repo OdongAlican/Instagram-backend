008e7313c60648dab8efd2f31c0d694a6f9b5269
OdongAlican <sandieo.2020@gmail.com>
2021-03-28 22:47:22 +0300

    update create and destroy response

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,8 +5,17 @@ class LikesController < ApplicationController
     @like = current_user.likes.build(like_params)
     @post = @like.post
     if @like.save
-      data = { message: 'Post Liked' }
-      json_response(data, :created)
+      # data = { message: 'Post Liked' }
+      # json_response(data, :created)
+      @posts = Post.all.limit(10).order('created_at DESC')
+      nonFollowedList = Post.currentUserFollowers(current_user['id'])
+      data = @posts.to_json({ include: ['user', 'photos',
+                                        { likes: { include: 'user' } },
+                                        { comments: { include: 'user' } },
+                                        { bookmarks: { include: 'user' } }] })
+  
+      final = { data: data, followeesList: nonFollowedList }
+      json_response(final, :created)
     else
       json_response({ message: 'Post already liked' })
     end
@@ -17,7 +26,16 @@ class LikesController < ApplicationController
     @post = @like.post
     if current_user.id == @like.user_id
       if @like.destroy
-        json_response({ message: 'Post disiked' }, :no_content)
+        # json_response({ message: 'Post disiked' }, :no_content)
+        @posts = Post.all.limit(10).order('created_at DESC')
+        nonFollowedList = Post.currentUserFollowers(current_user['id'])
+        data = @posts.to_json({ include: ['user', 'photos',
+                                          { likes: { include: 'user' } },
+                                          { comments: { include: 'user' } },
+                                          { bookmarks: { include: 'user' } }] })
+    
+        final = { data: data, followeesList: nonFollowedList }
+        json_response(final, :created)
       else
         json_response({ message: 'Something went wrong' })
       end
