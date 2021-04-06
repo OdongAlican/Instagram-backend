1acfe7b2d034f0a355caed91275aa3051b081c8c
OdongAlican <sandieo.2020@gmail.com>
2021-04-06 19:23:41 +0300

    update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,15 +5,13 @@ class LikesController < ApplicationController
     @like = current_user.likes.build(like_params)
     @post = @like.post
     if @like.save
-      # data = { message: 'Post Liked' }
-      # json_response(data, :created)
       @posts = Post.all.limit(10).order('created_at DESC')
       nonFollowedList = Post.currentUserFollowers(current_user['id'])
       data = @posts.to_json({ include: ['user', 'photos',
                                         { likes: { include: 'user' } },
                                         { comments: { include: 'user' } },
                                         { bookmarks: { include: 'user' } }] })
-  
+
       final = { data: data, followeesList: nonFollowedList }
       json_response(final, :created)
     else
@@ -26,14 +24,13 @@ class LikesController < ApplicationController
     @post = @like.post
     if current_user.id == @like.user_id
       if @like.destroy
-        # json_response({ message: 'Post disiked' }, :no_content)
         @posts = Post.all.limit(10).order('created_at DESC')
         nonFollowedList = Post.currentUserFollowers(current_user['id'])
         data = @posts.to_json({ include: ['user', 'photos',
                                           { likes: { include: 'user' } },
                                           { comments: { include: 'user' } },
                                           { bookmarks: { include: 'user' } }] })
-    
+
         final = { data: data, followeesList: nonFollowedList }
         json_response(final, :created)
       else
