5ae079e0197de3b588b4d971e25960b43cb8c435
OdongAlican <sandieo.2020@gmail.com>
2021-04-06 19:26:50 +0300

    update authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  # before_action :authorized
+  before_action :authorized
 
   include Response
   include ExceptionHandler
